20e46ec2ec9ff2570b364f135cfd130a1b568b58
click-SK <105132436+click-SK@users.noreply.github.com>
2023-01-10 22:22:58 +0200

    Add files via upload

---
+++ constants/basket.del/categories.js
@@ -0,0 +1,6 @@
+const catalogCat = [
+    "Ladies Watch",
+    "Man`s Watch",
+    "Smart Watch",
+    "Sports Watch",
+  ];
--- constants/root.js
+++ constants/root.js
@@ -1,5 +1,6 @@
 const ROOT_HEADER = document.getElementById('header')
 const ROOT_GOODS = document.getElementById('goods')
 const ROOT_PRODUCTS = document.getElementById('products')
+const ROOT_PRODUCTS_CAT = document.getElementById('products-cat')
 const ROOT_SHOPPING = document.getElementById('shopping')
 const ROOT_FOOTER = document.getElementById('footer')
