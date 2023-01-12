45c4b88d6e035d3fa8e68a804a6cdf892e830a40
click-SK <105132436+click-SK@users.noreply.github.com>
2023-01-12 23:27:25 +0200

    Add files via upload

--- components/header/header.js
+++ components/header/header.js
@@ -96,7 +96,7 @@ class Header{
           
 
           htmlNavbar += `
-            <ul class="header__item-nav--categories" onclick="headerPage.hendlerClickFilter()">
+            <ul class="header__item-nav--categories" ">
               ${ htmlSearchCategories}
             </ul>`
       
@@ -117,6 +117,10 @@ class Header{
 
             htmlHeader += `
             <header class="">
+            <div class= "header-burger">
+              <div class="header__burger-button"> ||| </div>
+              <div class="header__burger-logo"> LOGO</div>
+             </div>
             <div class=" header-item header__top">
                 <div class="header__logo"> LOGO</div>
                 <div class="header__search">
@@ -149,8 +153,8 @@ class Header{
                 <div class="header__item header__item--page"><a href="#">About Us</a></div>
                 <div class="header__item header__item--page"><a href="#">Payment and delivery</a></div>
                 <div class="header__item header__item--page"><a href="#">Exchange and return</a></div>
-                <div class="header__item header__item--page"><a href="#">Contacts</a></div>
-                <div id='basket' class="header__item header__item--basket" onclick="headerPage.handlerOpenShopingPage();"><svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" fill="currentColor" class="bi bi-basket3" viewBox="0 0 16 16">
+                <div class="header__item header__item--page header__item--page--active"><a href="#">Contacts</a></div>
+                <div id='basket' class="header__item header__item--basket"  onclick="headerPage.handlerOpenShopingPage();"><svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" fill="currentColor" class="bi bi-basket3" viewBox="0 0 16 16">
                     <path d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H.5a.5.5 0 0 1-.5-.5v-1A.5.5 0 0 1 .5 6h1.717L5.07 1.243a.5.5 0 0 1 .686-.172zM3.394 15l-1.48-6h-.97l1.525 6.426a.75.75 0 0 0 .729.574h9.606a.75.75 0 0 0 .73-.574L15.056 9h-.972l-1.479 6h-9.21z"/>
                   </svg> <a href="#">my basket</a>
                     <div id='count' class="counter"> ${count} </div> 
