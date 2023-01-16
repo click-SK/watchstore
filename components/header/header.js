25357e0dabc35e497f046208d708cd8fd191b897
click-SK <sashakushnir4991@gmail.com>
2023-01-16 13:45:45 +0200

    optional

--- components/header/header.js
+++ components/header/header.js
@@ -110,14 +110,9 @@ class Header{
 
             htmlHeader += `
             <header class="">
-            <div class= "header-burger " onclick = "headerPage.hendlerMenuBurger();">
-              <div class="header__burger-button"> <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
-              <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-            </svg> </div>
-              <div class="header__burger-logo"> LOGO</div>
-             </div>
+            
             <div class=" header-item header__top">
-                <div class="header__logo"> LOGO</div>
+                <div class="header__logo">GO-TIME</div>
                 <div class="header__search">
                         <div class="header__search-form">
                             <input class="header__search-input" type = "text" placeholder="Search">
