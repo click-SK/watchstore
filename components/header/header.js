d79378bed1bf172519351c6fff1f78c9d158108c
click-SK <sashakushnir4991@gmail.com>
2023-01-28 22:56:17 +0200

    rer

--- components/header/header.js
+++ components/header/header.js
@@ -159,7 +159,7 @@ class Header{
                         <div class="header__search-form">
                         
                             <input id="search-input" class="header__search-input" type = "text" placeholder="Search" oninput = "headerPage.searchGoods();">
-                            <div class="header__search-categories">
+                            <div class="header__search-categories hidden">
                               All Categorise 
                               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-compact-down" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M1.553 6.776a.5.5 0 0 1 .67-.223L8 9.44l5.776-2.888a.5.5 0 1 1 .448.894l-6 3a.5.5 0 0 1-.448 0l-6-3a.5.5 0 0 1-.223-.67z"/>
